27a7ea3220d88af17fed803efd60fd7a81efe47c
Gustavo <gustavolopez819@gmail.com>
2026-05-29 01:01:21 -0500

    feat(login): created login form ui

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -4,10 +4,11 @@ import { Hexagon } from "lucide-react";
 export default function LoginPage() {
 
   return (
-    <div className="max-w-xs w-full mx-auto px-4">
+    <div className="max-w-sm w-full mx-auto px-4 pt-28">
       <div className="flex flex-col items-center gap-2">
         <Hexagon />
-        <h1 className="text-2xl font-semibold">Accede a tu cuenta </h1>
+        <h1 className="text-2xl font-semibold">Bienvenido de Nuevo</h1>
+        <p className="text-sm text-muted-foreground">Inicia sesión para acceder a la plataforma</p>
       </div>
       <LoginForm />
     </div>
--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,33 +1,67 @@
 'use client'
 
 import { Button } from "@/src/shared/components/ui/button"
+import { Checkbox } from "@/src/shared/components/ui/checkbox"
 import { Input } from "@/src/shared/components/ui/input"
 import { Label } from "@/src/shared/components/ui/label"
+import { Separator } from "@/src/shared/components/ui/separator"
+import { Google } from "@/src/shared/svg/Google"
+import { ArrowUpRight } from "lucide-react"
+import Link from "next/link"
 
 export default function LoginForm() {
 
   return (
-    <div className="mt-6 space-y-3">
-      <div className="space-y-1">
+    <div className="mt-6 space-y-5">
+      <div>
+        <Button
+          variant={'outline'}
+          className="w-full gap-3"
+        >
+          <Google />
+          Continuar con Google
+        </Button>
+      </div>
+      <div className="flex justify-center gap-5 items-center overflow-hidden">
+        <Separator className="w-full bg-neutral-100" />
+        <span className="shrink-0 text-xs text-muted-foreground/60">o continua con tu e-mail</span>
+        <Separator className="w-full bg-neutral-100" />
+      </div>
+
+      <div className="space-y-2">
         <Label htmlFor="email">Correo</Label>
         <Input
           id="email"
           type="email"
-          placeholder="Email"
+          placeholder="juan@gmail.com"
         />
       </div>
-      <div className="space-y-1">
+      <div className="space-y-2">
         <Label htmlFor="password">Contraseña</Label>
         <Input
           id="password"
-          type="email"
-          placeholder="Password"
+          type="password"
+          placeholder="••••••••"
         />
+      </div>
+      <div className="flex justify-between text-sm">
+        <div className="flex items-center gap-1.5">
+          <Checkbox id="remember" />
+          <label htmlFor="remember">Recuerdame</label>
+        </div>
+        <Link href={'/auth/recover'} className="text-primary">Olvidaste tu contraseña?</Link>
       </div>
 
       <Button className="w-full">
         Acceder
       </Button>
+      <div className="flex justify-center gap-2 text-muted-foreground">
+        No tienes cuenta?
+        <span className="flex items-center gap-0.5 font-medium text-primary group">
+          <Link href={'/auth/register'}>Registrate</Link>
+          <ArrowUpRight className="size-3 stroke-3 group-hover:translate-x-0.5 transition-transform" />
+        </span>
+      </div>
     </div>
   )
 }
