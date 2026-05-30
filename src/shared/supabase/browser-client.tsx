5f8c80657b8200b65a08142f6d06adc36d019761
Gustavo <gustavolopez819@gmail.com>
2026-05-29 21:08:12 -0500

    feat(supabase): add supabase browser client

---
+++ src/shared/supabase/browser-client.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { createBrowserClient } from "@supabase/ssr";
+import { SupabaseClient } from "@supabase/supabase-js"
+import { Database } from "../types/database.types"
+
+let client: SupabaseClient<Database> | null = null;
+
+export function getSupabaseBrowserClient(): SupabaseClient<Database> {
+
+  if (client) {
+    return client;
+  }
+
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+  client = createBrowserClient<Database>(
+    supabaseUrl,
+    supabaseAnonKey
+  );
+
+  return client;
+}
