1c97fdfe3f37ea1c4a1e213d8836659343e92a73
Gustavo <gustavolopez819@gmail.com>
2026-06-08 12:40:20 -0500

    feat(avatar): add api route to change profile avatar

--- app/api/profiles/[id]/route.ts
+++ app/api/profiles/[id]/route.ts
@@ -21,13 +21,13 @@ export async function GET(
           return cookieStore.getAll();
         },
         setAll() {},
-      }
-    }
+      },
+    },
   );
 
   const { data, error } = await supabase
     .from("profiles")
-    .select('*')
+    .select("*")
     .eq("id", id)
     .single();
 
@@ -37,4 +37,41 @@ export async function GET(
   }
 
   return Response.json(data);
+}
+export async function PATCH(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
+  const { avatar_url } = await req.json();
+  console.log('Avatar: ', avatar_url);
+  if (!id) {
+    return Response.json({ error: "Id inválido" }, { status: 500 });
+  }
+
+  const cookieStore = await cookies();
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll() {},
+      },
+    },
+  );
+
+  const { error } = await supabase
+    .from("profiles")
+    .update({ avatar_url })
+    .eq("id", id);
+
+  if (error) {
+    console.error(error);
+    return Response.json({ error: error.message }, { status: 500 });
+  }
+  
+  return Response.json({ success: true });
 }
