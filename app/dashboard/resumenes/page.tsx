c1da14672b5924094262b9bc7cd45989f9a727fb
Gustavo <gustavolopez819@gmail.com>
2026-06-10 17:54:25 -0500

    feat(summaries): add feature based arquitecture

--- app/dashboard/resumenes/page.tsx
+++ app/dashboard/resumenes/page.tsx
@@ -1,9 +1,25 @@
-import { SummaryGenerator } from "@/src/shared/components/summaryGenerator";
+import SumConfig from "@/src/features/summaries/components/SumConfig";
+import SumFont from "@/src/features/summaries/components/SumFont";
+import SumHeader from "@/src/features/summaries/components/SumHeader";
+import SumPreview from "@/src/features/summaries/components/SumPreview";
 
 export default function ResumenesPage() {
   return (
-    <main className="min-h-screen bg-zinc-950 p-8">
-      <SummaryGenerator />
+    <main className="min-h-screen px-4">
+      <SumHeader />
+      <div className="w-full min-h-screen">
+        <div className="w-full">
+          <div className="grid gap-6 lg:grid-cols-[420px_minmax(0,1fr)]">
+            <div className="space-y-6">
+              <SumConfig />
+
+              <SumFont />
+            </div>
+
+            <SumPreview />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
