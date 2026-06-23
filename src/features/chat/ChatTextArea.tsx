fa8eb3068b6e5f1e126b83ad4f609388fe0cb874
Gustavo <gustavolopez819@gmail.com>
2026-06-22 22:47:09 -0500

    feat(chat): created textarea with btn

---
+++ src/features/chat/ChatTextArea.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { Button } from "@/src/shared/components/ui/button";
+import { Textarea } from "@/src/shared/components/ui/textarea";
+import { Paperclip, Send } from "lucide-react";
+
+export default function ChatTextArea() {
+
+  return (
+    <form className="relative">
+      <Textarea
+        className="w-175 h-30 p-4 border rounded-xl border-input focus-visible:border-muted-foreground/40 resize-none duration-300"
+        placeholder="Cual es tu pregunta de hoy..."
+      />
+
+      <div className="w-full absolute bottom-0 flex justify-between p-3">
+        <Button
+          size={'icon-lg'}
+          variant={'ghost'}
+          className="bg-muted hover:bg-muted! text-muted-foreground cursor-pointer"
+        >
+          <Paperclip className="size-4.5" />
+        </Button>
+
+        <Button
+          size={'icon-lg'}
+          variant={'ghost'}
+          className="bg-muted hover:bg-muted! text-muted-foreground cursor-pointer group"
+        >
+          <Send className="size-4.5" />
+        </Button>
+      </div>
+    </form>
+  )
+}
