ac7445deed4d68755a15a6f2568884f808282b44
Gustavo <gustavolopez819@gmail.com>
2026-06-22 23:55:42 -0500

    feat(chat): add on change prop to textarea

--- src/features/chat/ChatTextArea.tsx
+++ src/features/chat/ChatTextArea.tsx
@@ -7,12 +7,13 @@ import { useState } from "react";
 
 export default function ChatTextArea() {
   const [text, setText] = useState('');
-  
+
   return (
     <form className="relative">
       <Textarea
         className="w-175 h-30 p-4 border rounded-xl border-input focus-visible:border-muted-foreground/40 resize-none duration-300"
         placeholder="Cual es tu pregunta de hoy..."
+        onChange={(e) => setText(e.target.value)}
       />
 
       <div className="w-full absolute bottom-0 flex justify-between p-3">
