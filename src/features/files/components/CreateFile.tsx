350d283632b32c003663c4d174dd1c8968402e41
Gustavo <gustavolopez819@gmail.com>
2026-06-24 17:22:45 -0500

    feat(files): refactor CreateFile component to use Dialog for file upload

--- src/features/files/components/CreateFile.tsx
+++ src/features/files/components/CreateFile.tsx
@@ -1,16 +1,35 @@
+'use client'
+
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/src/shared/components/ui/dialog";
 import { FileText, Plus } from "lucide-react";
+import FileUpload from "./FileInput";
 
 export default function CreateFile() {
-  
+
   return (
-  <div className="p-4 border border-input rounded-xl hover:bg-muted relative group cursor-pointer">
-    <div className="absolute top-3 right-3 text-muted-foreground">
-      <Plus className="size-5.5 group-hover:size-4.5 transition-all duration-100" />
-    </div>
-    <div className="w-fit p-2 bg-muted group-hover:bg-primary rounded-lg group-hover:text-background transition-colors">
-      <FileText className="size-4.5" />
-    </div>
-    <p className="mt-3 font-semibold">Subir documento</p>
-  </div>  
-  )  
+    <Dialog>
+      <DialogTrigger asChild>
+        <button className="p-4 border border-input rounded-xl hover:bg-muted relative group cursor-pointer text-start">
+          <div className="absolute top-3 right-3 text-muted-foreground">
+            <Plus className="size-5.5 group-hover:size-4.5 transition-all duration-100" />
+          </div>
+          <div className="w-fit p-2 bg-muted group-hover:bg-primary rounded-lg group-hover:text-background transition-colors">
+            <FileText className="size-4.5" />
+          </div>
+          <p className="mt-3 font-semibold">Subir documento</p>
+        </button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Sube un archivo</DialogTitle>
+          <DialogDescription>Arrastra o selecciona un archivo</DialogDescription>
+        </DialogHeader>
+        <div>
+          <FileUpload />
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
 }
+
+
