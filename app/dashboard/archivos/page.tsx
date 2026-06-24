ed0292892f5ff31981a58c510e238bd5d0ced0bd
Gustavo <gustavolopez819@gmail.com>
2026-06-24 17:44:43 -0500

    feat(files): add craete folder btn

--- app/dashboard/archivos/page.tsx
+++ app/dashboard/archivos/page.tsx
@@ -1,4 +1,5 @@
 import CreateFile from "@/src/features/files/components/CreateFile"
+import { Folder, Plus } from "lucide-react"
 
 export default function ArchivosPage() {
 
@@ -6,6 +7,15 @@ export default function ArchivosPage() {
     <main>
       <div className="grid grid-cols-4 gap-5">
         <CreateFile />
+        <button className="p-4 border border-input rounded-xl hover:bg-muted relative group cursor-pointer text-start">
+          <div className="absolute top-3 right-3 text-muted-foreground">
+            <Plus className="size-5.5 group-hover:size-4.5 transition-all duration-100" />
+          </div>
+          <div className="w-fit p-2 bg-muted group-hover:bg-primary rounded-lg group-hover:text-white transition-colors">
+            <Folder className="size-4.5" />
+          </div>
+          <p className="mt-3 font-semibold">Crear carpeta</p>
+        </button>
       </div>
       <h1>Archivos</h1>
 
