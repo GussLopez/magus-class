e79dbdc0e49a8d525a0d3ac1875a19b271cb048b
Gustavo <gustavolopez819@gmail.com>
2026-06-24 17:06:08 -0500

    feat(files): add hover classes to create file card

--- src/features/files/components/CreateFile.tsx
+++ src/features/files/components/CreateFile.tsx
@@ -3,11 +3,11 @@ import { FileText, Plus } from "lucide-react";
 export default function CreateFile() {
   
   return (
-  <div className="p-4 border border-input rounded-xl hover:bg-muted relative group">
+  <div className="p-4 border border-input rounded-xl hover:bg-muted relative group cursor-pointer">
     <div className="absolute top-3 right-3 text-muted-foreground">
-      <Plus className="size-6 group-hover:size-5 transition-all duration-100" />
+      <Plus className="size-5.5 group-hover:size-4.5 transition-all duration-100" />
     </div>
-    <div className="w-fit p-2 bg-foreground rounded-lg text-background">
+    <div className="w-fit p-2 bg-muted group-hover:bg-primary rounded-lg group-hover:text-background transition-colors">
       <FileText className="size-4.5" />
     </div>
     <p className="mt-3 font-semibold">Subir documento</p>
