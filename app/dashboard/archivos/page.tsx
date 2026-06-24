548fbebd17e52738b8f09f74bf33f900081acf13
Gustavo <gustavolopez819@gmail.com>
2026-06-24 16:50:27 -0500

    feat(files): add archivos page

---
+++ app/dashboard/archivos/page.tsx
@@ -0,0 +1,14 @@
+import CreateFile from "@/src/features/files/components/CreateFile"
+
+export default function ArchivosPage() {
+
+  return (
+    <main>
+      <div className="grid grid-cols-4 gap-5">
+        <CreateFile />
+      </div>
+      <h1>Archivos</h1>
+
+    </main>
+  )
+}
