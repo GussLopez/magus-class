b042e19babc7f68e48cc1faf15e2eb3473803e76
Gustavo <gustavolopez819@gmail.com>
2026-06-24 17:30:04 -0500

    fix(styles): add hover text white class

--- src/features/files/components/CreateFile.tsx
+++ src/features/files/components/CreateFile.tsx
@@ -13,7 +13,7 @@ export default function CreateFile() {
           <div className="absolute top-3 right-3 text-muted-foreground">
             <Plus className="size-5.5 group-hover:size-4.5 transition-all duration-100" />
           </div>
-          <div className="w-fit p-2 bg-muted group-hover:bg-primary rounded-lg group-hover:text-background transition-colors">
+          <div className="w-fit p-2 bg-muted group-hover:bg-primary rounded-lg group-hover:text-white transition-colors">
             <FileText className="size-4.5" />
           </div>
           <p className="mt-3 font-semibold">Subir documento</p>
