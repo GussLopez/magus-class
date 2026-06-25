52beced6b4b1feb4a1f320d4aa3622ce902147a9
Gustavo <gustavolopez819@gmail.com>
2026-06-24 23:59:35 -0500

    fix(ArchivosPage): enhance button transition effects for improved UI responsiveness

--- app/dashboard/archivos/page.tsx
+++ app/dashboard/archivos/page.tsx
@@ -8,11 +8,11 @@ export default function ArchivosPage() {
     <main>
       <div className="grid grid-cols-4 gap-5">
         <CreateFile />
-        <button className="p-4 border border-input rounded-xl hover:bg-muted relative group cursor-pointer text-start">
+        <button className="p-4 border border-input rounded-xl hover:bg-muted relative group cursor-pointer text-start transition-colors duration-150">
           <div className="absolute top-3 right-3 text-muted-foreground">
-            <Plus className="size-5.5 group-hover:size-4.5 transition-all duration-100" />
+            <Plus className="size-5.5 group-hover:size-4.5 transition-all duration-150" />
           </div>
-          <div className="w-fit p-2 bg-muted group-hover:bg-primary rounded-lg group-hover:text-white transition-colors">
+          <div className="w-fit p-2 bg-muted group-hover:bg-primary rounded-lg group-hover:text-white transition-colors duration-150">
             <Folder className="size-4.5" />
           </div>
           <p className="mt-3 font-semibold">Crear carpeta</p>
