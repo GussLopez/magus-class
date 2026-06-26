02223908af17f4a8a97890740ac36e37fd52b321
Gustavo <gustavolopez819@gmail.com>
2026-06-26 12:56:55 -0500

    feat(ui): enhance TableFiles component with tabs and search functionality

--- src/features/files/components/TableFiles.tsx
+++ src/features/files/components/TableFiles.tsx
@@ -4,8 +4,10 @@ import { getSupabaseBrowserClient } from "@/src/shared/supabase/browser-client"
 import { useQuery } from "@tanstack/react-query"
 import { File } from "../files.types"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/src/shared/components/ui/table"
-import { EllipsisVertical, FileText } from "lucide-react"
+import { EllipsisVertical, FileText, ListFilter, Search } from "lucide-react"
 import { Button } from "@/src/shared/components/ui/button"
+import { Tabs, TabsList, TabsTab } from "@/src/shared/components/ui/tabs"
+import { Input } from "@/src/shared/components/ui/input"
 
 export default function TableFiles() {
   const supabase = getSupabaseBrowserClient()
@@ -31,12 +33,34 @@ export default function TableFiles() {
     retry: 1
   });
 
-  console.log(data);
   return (
     <div className="mt-8">
       <h2 className="font-medium">Todos los archivos</h2>
+      <div className="flex justify-between mt-3">
+        <div>
+          <Tabs>
+            <TabsList>
+              <TabsTab value={'all'}>Ver todos</TabsTab>
+              <TabsTab value={'documents'}>Documentos</TabsTab>
+              <TabsTab value={'pdfs'}>PDFs</TabsTab>
+              <TabsTab value={'others'}>Otros</TabsTab>
+            </TabsList>
+          </Tabs>
+        </div>
 
-      <div>
+        <div className="flex gap-3 max-h-10 overflow-hidden">
+          <div className="relative">
+            <Search className="absolute top-1/2 -translate-y-1/2 left-2 text-input" />
+            <Input className="h-full py-0 pl-10 border"/>
+          </div>
+
+          <Button variant={'outline'}>
+            <ListFilter />
+            Filtros
+          </Button>
+        </div>
+      </div>
+      <div className="mt-5">
         <Table>
           <TableHeader>
             <TableRow>
