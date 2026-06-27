5853a218f0bb13766d2fae7139276e792b6108b6
Gustavo <gustavolopez819@gmail.com>
2026-06-26 23:47:30 -0500

    feat(files): enhance delete file dialog with dropdown trigger and loading state

--- src/features/files/components/DeleteFileDialog.tsx
+++ src/features/files/components/DeleteFileDialog.tsx
@@ -1,27 +1,98 @@
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/src/shared/components/ui/alert-dialog";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/src/shared/components/ui/alert-dialog";
+import { DropdownMenuItem } from "@/src/shared/components/ui/dropdown-menu";
+import { Spinner } from "@/src/shared/components/ui/spinner";
+import { getSupabaseBrowserClient } from "@/src/shared/supabase/browser-client";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Trash2 } from "lucide-react";
+import { useState } from "react";
+import { sileo } from "sileo";
 
 interface DialogProps {
-  open: boolean;
-  setOpen: (open: boolean) => void;
+  documentId: string;
 }
 
-export default function DeleteFileDialog({ open, setOpen }: DialogProps) {
+export default function DeleteFileDialog({ documentId }: DialogProps) {
+  const [open, setOpen] = useState(false);
+  const queryClient = useQueryClient();
+  const supabase = getSupabaseBrowserClient();
+
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async () => {
+      const { data } = await supabase.auth.getSession();
+      const token = data.session?.access_token;
+
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/documents/${documentId}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+        }
+      );
+
+      if (!res.ok) {
+        throw new Error('Error al eliminar el documento');
+      }
+
+      return res.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user-files"] });
+      sileo.success({ title: 'Archvio eliminado correctamente' })
+      setOpen(false);
+    }
+  })
 
   return (
-   <AlertDialog open={open} onOpenChange={setOpen}>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>¿Estas completamente seguro?</AlertDialogTitle>
-          <AlertDialogDescription>
-            Esta acción no se puede deshacer. Esto eliminará permanentemente tu
-            archivo de nuestros servidores.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancelar</AlertDialogCancel>
-          <AlertDialogAction variant={'destructive'}>Eliminar</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+    <>
+      <DropdownMenuItem
+        variant="destructive"
+        onSelect={(e) => {
+          e.preventDefault();
+          setOpen(true);
+        }}
+      >
+        <Trash2 />
+        Eliminar
+      </DropdownMenuItem>
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              ¿Estás completamente seguro?
+            </AlertDialogTitle>
+
+            <AlertDialogDescription>
+              Esta acción no se puede deshacer. Esto eliminará permanentemente tu
+              archivo de nuestros servidores.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+
+          <AlertDialogFooter>
+            <AlertDialogCancel disabled={isPending}>
+              Cancelar
+            </AlertDialogCancel>
+
+            <AlertDialogAction
+              variant="destructive"
+              disabled={isPending}
+              onClick={(e) => {
+                e.preventDefault();
+                mutate();
+              }}
+            >
+              {isPending ? (
+                <>
+                  <Spinner />
+                  Eliminando
+                </>
+              ) : "Eliminar"}
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   )
 }
