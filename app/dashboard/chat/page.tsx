634578bce1bf4e02e1b3906dc45c08583a32186a
Gustavo <gustavolopez819@gmail.com>
2026-06-27 21:30:52 -0500

    feat(chat): update ChatPage layout and remove unnecessary heading

--- app/dashboard/chat/page.tsx
+++ app/dashboard/chat/page.tsx
@@ -3,9 +3,8 @@ import ChatTextArea from "@/src/features/chat/components/ChatTextArea";
 export default function ChatPage() {
 
   return (
-    <main className="min-h-full flex flex-col justify-center items-center">
+    <main className="min-h-full flex flex-col justify-center items-center relative">
       <div className="flex flex-col gap-4">
-        <h1 className="text-[32px] font-semibold text-center">¿Por donde quiere comenzar?</h1>
         <ChatTextArea />
       </div>
     </main>
