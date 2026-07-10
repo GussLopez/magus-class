3bf603ee06994ceba09654f2b11afee25af1da3b
Gustavo <gustavolopez819@gmail.com>
2026-07-10 00:24:03 -0500

    feat(utils): add format Date function

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(date: string) {
+  return new Intl.DateTimeFormat('es-MX', {
+    dateStyle: 'medium',
+    timeStyle: 'short',
+  }).format(new Date(date))
+}
