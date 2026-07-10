2a034c4618d5bd5b315d3e91e3c2778c628a8b1d
Gustavo <gustavolopez819@gmail.com>
2026-07-09 23:09:40 -0500

    feat(files): create static file card component

---
+++ src/features/files/components/FileCard.tsx
@@ -0,0 +1,50 @@
+import { PDFIlustration } from "@/src/shared/components/icons/PdfIlustration";
+import { Button } from "@/src/shared/components/ui/button";
+import { EllipsisVertical, Star } from "lucide-react";
+
+export default function FileCard() {
+
+  return (
+    <div className="p-5 rounded-xl bg-card relative">
+      <div className="absolute top-5 w-full px-5 translate-x-1/2 right-1/2 flex justify-between">
+        <div>
+          <Button
+            size={'icon'}
+            variant={'ghost'}
+          >
+            <Star />
+          </Button>
+        </div>
+
+        <div>
+          <Button
+            size={'icon'}
+            variant={'ghost'}
+          >
+            <EllipsisVertical />
+          </Button>
+        </div>
+      </div>
+      <div className="border-b border-input">
+        <div className="flex justify-center items-center pt-8 pb-5">
+          <PDFIlustration className="size-18" />
+        </div>
+        <div className="text-center pb-5">
+          <p className="font-medium">License Agreement on Waterfall</p>
+          <span className="text-sm text-muted-foreground">INC.pdf</span>
+        </div>
+      </div>
+
+      <div className="flex justify-between pt-3">
+        <div>
+          <p className="text-sm">Tamaño:</p>
+          <span className="text-xs text-muted-foreground">2.3 MB</span>
+        </div>
+
+        <div className="w-10 h-10 flex justify-center items-center rounded-xl font-semibold bg-muted">
+          G
+        </div>
+      </div>
+    </div>
+  )
+}
